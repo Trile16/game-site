8c0905baa26eae5591cd6f4d6b123b834e7ed5e4
Tri Le <16trile@gmail.com>
2025-11-10 21:08:52 -0800

    UI updates

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,15 +4,19 @@ import ghLogo from "./assets/GitHub-Logo.png";
 import lLogo from "./assets/LinkedIn-Logo.png";
 
 function Footer(props) {
-  return (
+  return (<>
     <div id="footer">
-      <a href="https://github.com/Trile16" target="_blank">
-        <img className="icon" src={ghLogo} alt="GitHub Logo" />
-      </a>
-      <a href="https://www.linkedin.com/in/trile16/" target="_blank">
-        <img className="icon" src={lLogo} alt="LinkedIn Logo" />
-      </a>
+      <div id="footer-icons">
+        <a href="https://github.com/Trile16" target="_blank">
+          <img className="icon" src={ghLogo} alt="GitHub Logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/trile16/" target="_blank">
+          <img className="icon" src={lLogo} alt="LinkedIn Logo" />
+        </a>
+      </div>
+      <p>&copy; 2025 by Tri Le.</p>
     </div>
+  </>
   );
 }
 
